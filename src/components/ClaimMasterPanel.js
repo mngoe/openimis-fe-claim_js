0ffa9f83e89c16e7bd73a272a04b263547c455e1
kouamkouammichellewis <kouamkouammichellewis@gmail.com>
2023-10-03 14:38:29 +0100

    feature-26848

--- src/components/ClaimMasterPanel.js
+++ src/components/ClaimMasterPanel.js
@@ -227,6 +227,7 @@ class ClaimMasterPanel extends FormPanel {
                 reset={reset}
                 onChange={(d) => this.updateAttribute("dateTo", d)}
                 readOnly={ro}
+                required={true}
                 minDate={edited.dateFrom}
                 maxDate={edited.dateClaimed}
               />
