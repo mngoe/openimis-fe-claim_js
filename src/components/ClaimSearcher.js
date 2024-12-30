759054505353e630d85cc5720b2bbde725cf392d
michel-lewis <kouamkouammichellewis@gmail.com>
2024-12-30 18:09:10 +0100

    fix: correction on filters components

--- src/components/ClaimSearcher.js
+++ src/components/ClaimSearcher.js
@@ -46,7 +46,8 @@ class ClaimSearcher extends Component {
     this.extFields = props.modulesManager.getConf("fe-claim", "extFields", []);
     this.showOrdinalNumber = props.modulesManager.getConf("fe-claim", "claimForm.showOrdinalNumber", false);
   }
-  componentDidUpdate(prevProps, prevState, snapshot) {
+ 
+  fetchWithInitialFilters (filters){
     if (this.state.searchInitiated == false && !!this.state.initialFitlers) {
       console.log("enter filter ", this.state.initialFitlers);
       this.onFiltersApplied(this.state.initialFitlers);
@@ -299,10 +300,21 @@ class ClaimSearcher extends Component {
   };
 
   onFiltersApplied = (filters) => {
-    this.setState({
-      searchInitiated: true,
-      filters,
-    });
+    if (this.state.searchInitiated == false && !!this.state.initialFitlers) {
+      console.log("enter filter ", this.state.initialFitlers);
+      let filters = this.state.initialFitlers
+      this.setState({
+        searchInitiated: true,
+        filters,
+      });
+      
+    }else {
+      this.setState({
+        searchInitiated: true,
+        filters,
+      });
+    }
+   
   };
 
   isClaimNotRestored = (_, claim) => this.state.showRestored && !claim?.restore;
