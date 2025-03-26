56517a3d694bc0d85641180f1f9082c833c3b61f
vaneck237 <vaneckbilongo@gmail.com>
2025-03-26 18:01:04 +0100

    fix bug

--- src/components/ClaimMasterPanel.js
+++ src/components/ClaimMasterPanel.js
@@ -244,7 +244,7 @@ class ClaimMasterPanel extends FormPanel {
           activeOrInactivePolicies.push(policy)
         }
         if(activeOrInactivePolicies.length > 1){
-          alert(formatMessage(intl, "claim", "edit.multipleCsPolicies"))
+          policyNumber = "";
         } else if(activeOrInactivePolicies.length == 1){
           policyNumber = activeOrInactivePolicies[0].policy.policyNumber;
         }
