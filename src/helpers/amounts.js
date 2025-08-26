2458416e222334aae43a9406065f3c5be40fe23c
vaneck237 <vaneckbilongo@gmail.com>
2025-08-26 16:11:06 +0100

    fix service & items modification in claim

--- src/helpers/amounts.js
+++ src/helpers/amounts.js
@@ -8,7 +8,7 @@ export function claimedAmount(r) {
         if(Object?.keys(r.service)?.length!=0){
           let currentPackageType = r.service.packagetype;
           if(currentPackageType=="S"){
-            totalPrice += parseFloat(r.service.price);
+            totalPrice += (r?.qtyProvided * parseFloat(r.priceAsked));
           }else{
             if(r?.service.manualPrice){
               totalPrice += parseFloat(r.service.price);
