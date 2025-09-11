2c7284f730f13eab8173f0980ef38d1f5a8c7474
Iandraina <217048206+Iandraina-etech@users.noreply.github.com>
2025-09-11 13:19:44 +0300

    Update ClaimMainMenu.js

--- src/menus/ClaimMainMenu.js
+++ src/menus/ClaimMainMenu.js
@@ -5,53 +5,70 @@ import { Keyboard, ScreenShare, Assignment } from "@material-ui/icons";
 import { formatMessage, MainMenuContribution, withModulesManager } from "@openimis/fe-core";
 import { RIGHT_ADD, RIGHT_SUBMIT, RIGHT_CLAIMREVIEW, RIGHT_PROCESS } from "../constants";
 const CLAIM_MAIN_MENU_CONTRIBUTION_KEY = "claim.MainMenu";
+const ADMIN_MAIN_MENU_CONTRIBUTION_KEY = "admin.MainMenu";
 
 class ClaimMainMenu extends Component {
   render() {
     const { rights } = this.props;
-    let entries = [];
+    let claimEntries = [];
+    let adminEntries = [];
+
+    // Claim entries
     if (!!rights.filter((r) => r >= RIGHT_ADD && r <= RIGHT_SUBMIT).length) {
-      // RIGHT_SEARCH is shared by HF & HQ staff)
-      entries.push({
+      claimEntries.push({
         text: formatMessage(this.props.intl, "claim", "menu.healthFacilityClaims"),
         icon: <Keyboard />,
         route: "/claim/healthFacilities",
       });
     }
     if (!!rights.filter((r) => r >= RIGHT_CLAIMREVIEW && r <= RIGHT_PROCESS).length) {
-      entries.push({
+      claimEntries.push({
         text: formatMessage(this.props.intl, "claim", "menu.reviews"),
         icon: <Assignment />,
         route: "/claim/reviews",
       });
     }
-    entries.push(
+    claimEntries.push(
       ...this.props.modulesManager
         .getContribs(CLAIM_MAIN_MENU_CONTRIBUTION_KEY)
         .filter((c) => !c.filter || c.filter(rights)),
     );
+
+    // Admin entries (specialities & prescribers)
     if (!!rights.filter((r) => r >= RIGHT_CLAIMREVIEW && r <= RIGHT_PROCESS).length) {
-      entries.push({
+      adminEntries.push({
         text: formatMessage(this.props.intl, "claim", "menu.specialities"),
         icon: <Assignment />,
         route: "/claim/specialities",
       });
-    }
-    if (!!rights.filter((r) => r >= RIGHT_CLAIMREVIEW && r <= RIGHT_PROCESS).length) {
-      entries.push({
+      adminEntries.push({
         text: formatMessage(this.props.intl, "claim", "menu.prescribers"),
         icon: <Assignment />,
         route: "/claim/prescribers",
       });
     }
-    if (!entries.length) return null;
+
     return (
-      <MainMenuContribution
-        {...this.props}
-        header={formatMessage(this.props.intl, "claim", "mainMenu")}
-        icon={<ScreenShare />}
-        entries={entries}
-      />
+      <>
+        {!!claimEntries.length && (
+          <MainMenuContribution
+            {...this.props}
+            header={formatMessage(this.props.intl, "claim", "mainMenu")}
+            icon={<ScreenShare />}
+            entries={claimEntries}
+          />
+        )}
+
+        {!!adminEntries.length && (
+          <MainMenuContribution
+            {...this.props}
+            contributionKey={ADMIN_MAIN_MENU_CONTRIBUTION_KEY}
+            header={formatMessage(this.props.intl, "admin", "mainMenu")}
+            icon={<Assignment />}
+            entries={adminEntries}
+          />
+        )}
+      </>
     );
   }
 }
