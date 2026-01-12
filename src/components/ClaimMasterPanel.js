5b8bbea97d8921002b265df79b32d56caa734e65
vaneck237 <vaneckbilongo@gmail.com>
2026-01-12 16:33:40 +0100

    add pregnancyAge field

--- src/components/ClaimMasterPanel.js
+++ src/components/ClaimMasterPanel.js
@@ -22,7 +22,8 @@ import { claimedAmount, approvedAmount } from "../helpers/amounts";
 import {
   claimHealthFacilitySet,
   clearClaim,
-  validateClaimCode
+  validateClaimCode,
+  fetchPregnancyAge
 } from "../actions";
 import ClaimStatusPicker from "../pickers/ClaimStatusPicker";
 import FeedbackStatusPicker from "../pickers/FeedbackStatusPicker";
@@ -46,7 +47,8 @@ class ClaimMasterPanel extends FormPanel {
     claimCodeError: null,
     codeClaim: null,
     claimPrefix: null,
-    policyNumber: null
+    policyNumber: null,
+    csProdId: null
   };
 
   constructor(props) {
@@ -127,6 +129,9 @@ class ClaimMasterPanel extends FormPanel {
 
     if (programName == "Chèque Santé" || programName == "Cheque Santé" || programName == "Ch\u00e8que Sant\u00e9") {
       let activeOrInactivePolicies = [];
+      var productId = "";
+      var dateTo = !!edited && edited.dateTo != undefined ? edited.dateTo : "";
+      var familyId = !!edited && edited.insuree != undefined ? edited.insuree.family.id : "";
       insureePolicies.forEach(function (policy) {
         if (policy.policy.effectiveDate <= edited.dateFrom && policy.policy.expiryDate >= edited.dateFrom ){ 
             if ((policy.policy.status == 2 || policy.policy.status == 8) && policy.policy.policyNumber != null) {
@@ -137,8 +142,10 @@ class ClaimMasterPanel extends FormPanel {
       var length = activeOrInactivePolicies.length
       if( length > 1){
         policyNumber = activeOrInactivePolicies[length - 1].policy.policyNumber
+        productId = activeOrInactivePolicies[length - 1].policy.product.id;
       } else if(length == 1){
         policyNumber = activeOrInactivePolicies[0].policy.policyNumber;
+        productId = activeOrInactivePolicies[0].policy.product.id;
       }else{
         policyNumber = ""
       }
@@ -146,6 +153,7 @@ class ClaimMasterPanel extends FormPanel {
         v = policyNumber + v
       }
       edited[`prefix`] = policyNumber
+      this.props.fetchPregnancyAge(dateTo, familyId, productId);
     } else {
       var programCode = this.props.edited.program ? this.props.edited.program.code.substring(0, 3) : "";
       var dateTo = this.props.edited.dateTo ? this.props.edited.dateTo.substring(0, 4) : "";
@@ -226,7 +234,8 @@ class ClaimMasterPanel extends FormPanel {
       restore,
       isRestored,
       isDuplicate,
-      changeProgram
+      resetServicesItems, 
+      pregnancyAge
     } = this.props;
     const {policyNumber, codeClaim, claimCode, claimPrefix, claimCodeError} = this.state;
     if (!edited) return null;
@@ -415,7 +424,7 @@ class ClaimMasterPanel extends FormPanel {
                 onChange={(v) => {
                   this.debounceUpdateCode("");
                   this.onChangeValue("program", v);
-                  changeProgram();
+                  resetServicesItems();
                 }}
                 required={true}
               />
@@ -463,6 +472,24 @@ class ClaimMasterPanel extends FormPanel {
             />
           )
         }
+        {chequeNumber != undefined && chequeNumber != null && pregnancyAge != undefined && pregnancyAge != null && (
+          <ControlledField
+            module="policy"
+            id="Claim.policyPregnancyAge"
+            field={
+              <Grid item xs={2} className={classes.item}>
+                <TextInput
+                  module="policy"
+                  label="policy.PregnancyAge"
+                  name="pregnancyAge"
+                  value={pregnancyAge}
+                  readOnly={true}
+                  reset={reset}
+                />
+              </Grid>
+            }
+          />
+        )}
         {chequeNumber != undefined && chequeNumber != null && (
           <ControlledField
             module="policy"
@@ -763,6 +790,7 @@ const mapStateToProps = (state) => ({
   isCodeValid: state.claim.validationFields?.claimCode?.isValid,
   isCodeValidating: state.claim.validationFields?.claimCode?.isValidating,
   codeValidationError: state.claim.validationFields?.claimCode?.validationError,
+  pregnancyAge: state.claim.pregnancyAge
 });
 
 const mapDispatchToProps = (dispatch) => {
@@ -771,6 +799,7 @@ const mapDispatchToProps = (dispatch) => {
       claimHealthFacilitySet,
       clearClaim,
       validateClaimCode,
+      fetchPregnancyAge
     },
     dispatch,
   );
