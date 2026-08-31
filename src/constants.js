b9cc5ef54bd2ea0a2be05d377d45600ae86e674a
vaneck237 <vaneckbilongo@gmail.com>
2026-08-31 11:09:17 +0100

    remove duplicate constant

--- src/constants.js
+++ src/constants.js
@@ -89,6 +89,5 @@ export const REVIEW_STATUS_DONE = 8;
 export const REVIEW_STATUS_BYPASS = 16;
 export const AUDIT_STATUS = ["A","R"];
 export const AUDIT_REJECTION_MOTIF = [1,2,3,4,5,6,7,8,9,10];
-export const STATUS_AUDITED = 64
 export const AUDIT_STATUS_ADOPTED = "A";
 export const AUDIT_STATUS_REJECTED = "R";
