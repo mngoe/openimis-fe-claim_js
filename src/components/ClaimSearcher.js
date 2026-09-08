2514fbc6dd4a62be082681694626ef5ac93217f2
Paul Sandjong <princesandjong777@gmail.com>
2026-09-08 12:51:14 +0100

    use formatted amounts (thousand separator, no decimals) in claims

--- src/components/ClaimSearcher.js
+++ src/components/ClaimSearcher.js
@@ -127,7 +127,7 @@ class ClaimSearcher extends Component {
                 claimed: (
                   <b>
                     {formatAmount(
-                      this.props.intl,
+                      this.props.modulesManager, this.props.intl,
                       selection.reduce((acc, v) => {
                         if (v.claimed) {
                           return acc + parseFloat(v.claimed);
@@ -149,7 +149,7 @@ class ClaimSearcher extends Component {
                 approved: (
                   <b>
                     {formatAmount(
-                      this.props.intl,
+                      this.props.modulesManager, this.props.intl,
                       selection.reduce((acc, v) => {
                         if (v.approved) {
                           return acc + parseFloat(v.approved);
@@ -257,8 +257,8 @@ class ClaimSearcher extends Component {
       (c) => formatDateFromISO(this.props.modulesManager, this.props.intl, c.dateProcessed),
       (c) => this.feedbackColFormatter(c),
       (c) => this.reviewColFormatter(c),
-      (c) => formatAmount(this.props.intl, c.claimed),
-      (c) => formatAmount(this.props.intl, c.approved),
+      (c) => formatAmount(this.props.modulesManager, this.props.intl, c.claimed),
+      (c) => formatAmount(this.props.modulesManager, this.props.intl, c.approved),
       (c) => formatMessage(this.props.intl, "claim", `claimStatus.${c.status}`),
     ];
     if (this.showPreAuthorization) {
